d37346aac1b2bfde1c225255c8c803a2dc90a0f6
andrii.chumak <andrexx@gmail.com>
2012-07-22 01:32:17 +0300

    Project sources

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace FsUaDownloader
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				ShowError("Filename with links not specified.");
+				return;
+			}
+
+			var filename = args[0];
+			if (!File.Exists(filename))
+			{
+				ShowError("File '" + filename + "' not exists.");
+				return;
+			}
+
+			Console.WriteLine("Load and parse links for download...");
+			var availableLinks = new Dictionary<string, string>();
+			var lines = File.ReadAllLines(filename);
+			foreach (var line in lines)
+			{
+				var tempLine = line.Trim();
+				if (String.IsNullOrWhiteSpace(tempLine))
+					continue;
+				if (!tempLine.StartsWith("http://"))
+					continue;
+				var name = Path.GetFileName(tempLine);
+				if (String.IsNullOrWhiteSpace(name))
+					continue;
+
+				availableLinks.Add(tempLine, name.Replace("+", " "));
+			}
+
+			if (availableLinks.Count == 0)
+			{
+				ShowError("Not found any links to download.");
+				return;
+			}
+
+			try
+			{
+				Console.WriteLine("Found " + availableLinks.Count + " files for downloading.");
+
+				var wc = new WebClient();
+
+				foreach (var link in availableLinks)
+				{
+					Console.WriteLine("Starting downloading: " + link.Value);
+					// not use async for special reasons
+					wc.DownloadFile(link.Key, link.Value);
+					Console.WriteLine("Completed downloading: " + link.Value);
+					Console.WriteLine();
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Exception occured during download. Review StackTrace below:");
+				ShowError(ex.ToString());
+				return;
+			}
+
+			Console.WriteLine("All downloads completed... Press Enter for exit.");
+			Console.ReadLine();
+		}
+
+		private static void ShowError(string message)
+		{
+			if (String.IsNullOrWhiteSpace(message))
+				return;
+
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine(message);
+			Console.ResetColor();
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FsUaDownloader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FsUaDownloader")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("d9c82278-5cc7-441b-bcfa-d31662589671")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
